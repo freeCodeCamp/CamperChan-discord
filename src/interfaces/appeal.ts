5f19485bc2051223662c099040cf14c5696f1f27
Naomi Carrigan <naomi@freecodecamp.org>
2025-12-09 11:46:25 -0800

    feat: update ban appeals (#935)

--- src/interfaces/appeal.ts
+++ src/interfaces/appeal.ts
@@ -1,21 +1,11 @@
 /* eslint-disable @typescript-eslint/naming-convention -- This is what we get.*/
 export interface Appeal {
-  table_id:     number;
-  database_id:  number;
-  workspace_id: number;
-  event_id:     string;
-  event_type:   string;
-  items: Array<
-    {
-      "id":              number;
-      "order":           string;
-      "User ID":         string;
-      "Username":        string;
-      "Code of Conduct": boolean;
-      "Reason":          string;
-      "Fair":            string;
-      "Improve":         string;
-      "Email":           string;
-    }
-  >;
+  id:              number;
+  manualSort:      number;
+  Discord_ID:      string;
+  Username:        string;
+  Code_of_Conduct: boolean;
+  Reason:          string;
+  Fairness:        string;
+  Improvement:     string;
 }
