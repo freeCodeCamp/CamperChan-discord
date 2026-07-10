bc26e8de3f07d8c24ab5945aa3421b7f5de44913
Naomi Carrigan <commits+fcc@nhcarrigan.com>
2026-07-10 10:56:51 -0700

    fix(tests): update tests with roles prop

--- test/commands/subcommands/github/handleAddLabels.spec.ts
+++ test/commands/subcommands/github/handleAddLabels.spec.ts
@@ -2,12 +2,23 @@ import { PermissionFlagsBits } from "discord.js";
 import { describe, assert, it } from "vitest";
 import { handleAddLabels }
   from "../../../../src/commands/subcommands/github/handleAddLabels.js";
+import { sprintReviewerRoleId } from "../../../../src/config/roles.js";
 
 describe("add labels handler", () => {
   it("does not allow non-moderators permission", () => {
     assert.isFalse(
       handleAddLabels.permissionValidator({
         permissions: new Set([ PermissionFlagsBits.SendMessages ]),
+        roles:       { cache: new Set([ ]) },
+      } as never),
+    );
+  });
+
+  it("allows sprint reviewers permission", () => {
+    assert.isTrue(
+      handleAddLabels.permissionValidator({
+        permissions: new Set([ PermissionFlagsBits.SendMessages ]),
+        roles:       { cache: new Set([ sprintReviewerRoleId ]) },
       } as never),
     );
   });
@@ -16,6 +27,7 @@ describe("add labels handler", () => {
     assert.isTrue(
       handleAddLabels.permissionValidator({
         permissions: new Set([ PermissionFlagsBits.ModerateMembers ]),
+        roles:       { cache: new Set([ sprintReviewerRoleId ]) },
       } as never),
     );
   });
@@ -24,6 +36,7 @@ describe("add labels handler", () => {
     assert.isTrue(
       handleAddLabels.permissionValidator({
         permissions: new Set([ PermissionFlagsBits.KickMembers ]),
+        roles:       { cache: new Set([ sprintReviewerRoleId ]) },
       } as never),
     );
   });
@@ -32,6 +45,7 @@ describe("add labels handler", () => {
     assert.isTrue(
       handleAddLabels.permissionValidator({
         permissions: new Set([ PermissionFlagsBits.BanMembers ]),
+        roles:       { cache: new Set([ sprintReviewerRoleId ]) },
       } as never),
     );
   });
