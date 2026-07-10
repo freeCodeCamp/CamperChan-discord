6253a2211f49d9db845a7feefcdea38aaf627e5b
Naomi Carrigan <commits+fcc@nhcarrigan.com>
2026-07-10 10:23:16 -0700

    feat: allow sprint reviewers to manage labels

--- src/config/roles.ts
+++ src/config/roles.ts
@@ -1,4 +1,5 @@
 const supporterRoleId = "1180246821231132763";
 const authorRoleId = "1220944327732101202";
+const sprintReviewerRoleId = "1524515894297100429";
 
-export { supporterRoleId, authorRoleId };
+export { supporterRoleId, authorRoleId, sprintReviewerRoleId };
